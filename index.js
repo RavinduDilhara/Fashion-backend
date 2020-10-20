bb9ba5deaa2acb220a139280bf718a66718feeb8
RavinduDilhara <ravindu.1dilhara@gmail.com>
2020-10-20 21:24:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+const uri = process.env.ATLAS_URI;
+
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB database connection established successfully'))
+    .catch(err => console.log(err));
+
+app.listen(port, err => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Server is running on port: ' + port);
+});
